0af7f241cc05cc77c1bebcf8fcf50f40db03f2ed
fabianwolski <fabianwolski03@gmail.com>
2025-05-03 14:24:39 +0100

    User Input + File Creation

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("generate irish rail timetable");
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter station code-defaults to ENFLD: ");
+            String code = scanner.nextLine().trim();
+
+            if (code.isEmpty()) {
+                code = "ENFLD";
+                System.out.print("Enter time window in minutes-defaults to 90(90 is max): ");
+                String mins = scanner.nextLine().trim();
+                if (mins.isEmpty()) {
+                    mins = "90";
+                }
+                scanner.close();
+
+                //fetch data
+                //create HTML+save file
+            }
+        } catch (Exception e) {
+            System.err.println("error: " + e.getMessage());
         }
     }
+    //open xml auto in browser????
 }
